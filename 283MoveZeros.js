59938fa84e9a145099fd9272132501c4c0808b31
shivakumarm125 <shivakumarm.2501@gmail.com>
2025-03-01 22:11:57 +0000

    Create 283MoveZeros.js

---
+++ 283MoveZeros.js
@@ -0,0 +1,28 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+
+
+var moveZeroes = function(nums) {
+    var nonZeroIndex = 0;
+
+    for(let i = 0; i<nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[nonZeroIndex], nums[i]] = [nums[i], nums[nonZeroIndex]];
+            nonZeroIndex++
+        }
+    }
+    return nums;
+};
